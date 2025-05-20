aeb483c42adf5e021533f126d1f8d8a31736fed9
daraccrafter <darkovujica01@gmail.com>
2025-05-20 09:12:46 +0200

    wip

--- plugins/sql/src/wrapper.rs
+++ plugins/sql/src/wrapper.rs
@@ -19,7 +19,7 @@ use sqlx::MySql;
 use sqlx::Postgres;
 #[cfg(feature = "sqlite")]
 use sqlx::Sqlite;
-use sqlx::{pool::PoolOptions, ConnectOptions};
+use sqlx::{pool::PoolOptions, SqliteConnectOptions, SqlitePoolOptions};
 
 use crate::LastInsertId;
 
@@ -89,7 +89,7 @@ impl DbPool {
 
                 if !Sqlite::database_exists(conn_url).await.unwrap_or(false) {
                     if let Some(key) = encryption_key {
-                        let _ = ConnectOptions::from_str(conn_url)?
+                        let _ = SqliteConnectOptions::from_str(conn_url)?
                             .pragma("key", key)
                             .create_if_missing(true)
                             .connect()
